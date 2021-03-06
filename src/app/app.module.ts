ccdd1fa381f910f38e6a581ef0d484f39f92d8dc
kkaazziimm <selman_hanci@hotmail.com>
2021-03-06 15:12:50 +0300

    Create: list-items component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from "@angular/router";
 import { NgxsModule } from "@ngxs/store";
 
 import { AppComponent } from "./app.component";
+import { ListItemsComponent } from "./modules/list-items/list-items.component";
+
+const appRoutes: Routes = [{ path: "", component: ListItemsComponent }];
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, NgxsModule.forRoot([])],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([]),
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [AppComponent, ListItemsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/modules/list-items/list-items.component.ts
+++ src/app/modules/list-items/list-items.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-showing-list",
-  templateUrl: "./showing-list.component.html",
-  styleUrls: ["./showing-list.component.scss"]
+  selector: "app-list-items",
+  templateUrl: "./list-items.component.html",
+  styleUrls: ["./list-items.component.scss"]
 })
-export class ShowingListComponent implements OnInit {
+export class ListItemsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
